8bc148ca318ba27e56803afa8979ec0f9d239dcf
Pinary-Pi <githubpinary@outlook.com>
2024-06-25 16:21:50 -0700

    Begin Fabric 1.21

---
+++ f1.21/src/main/java/net/pinary_pi/coloredbricks/ColoredBricksClient.java
@@ -0,0 +1,10 @@
+package net.pinary_pi.coloredbricks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ColoredBricksClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(){
+        
+    }
+}
