13384980a593f02febdd21e74da8e27981a5b927
Pinary-Pi <githubpinary@outlook.com>
2024-06-26 17:28:45 -0700

    Slab, Stair, Wall, and Chiseled Block Recipes

--- f1.21/src/main/java/net/pinary_pi/coloredbricks/data/ModRecipesProvider.java
+++ f1.21/src/main/java/net/pinary_pi/coloredbricks/data/ModRecipesProvider.java
@@ -18,6 +18,7 @@ import net.pinary_pi.coloredbricks.setup.ModItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.Items;
+import net.minecraft.recipe.Ingredient;
 import net.minecraft.recipe.book.RecipeCategory;
 
 import java.util.List;
@@ -67,6 +68,64 @@ public class ModRecipesProvider extends FabricRecipeProvider {
                                                                                                             Pair.of(ModItems.GRAY_BRICK, ModBlocks.GRAY_BRICKS),
                                                                                                             Pair.of(ModItems.BLACK_BRICK, ModBlocks.BLACK_BRICKS),
                                                                                                             Pair.of(ModItems.RED_BRICK, ModBlocks.RED_BRICKS));
+    private static final List<ItemConvertible> BLOCKS = List.of(ModBlocks.WHITE_BRICKS,
+                                                                ModBlocks.ORANGE_BRICKS,
+                                                                ModBlocks.PINK_BRICKS,
+                                                                ModBlocks.YELLOW_BRICKS,
+                                                                ModBlocks.LIME_BRICKS,
+                                                                ModBlocks.GREEN_BRICKS,
+                                                                ModBlocks.LIGHT_BLUE_BRICKS,
+                                                                ModBlocks.CYAN_BRICKS,
+                                                                ModBlocks.BLUE_BRICKS,
+                                                                ModBlocks.MAGENTA_BRICKS,
+                                                                ModBlocks.PURPLE_BRICKS,
+                                                                ModBlocks.BROWN_BRICKS,
+                                                                ModBlocks.LIGHT_GRAY_BRICKS,
+                                                                ModBlocks.GRAY_BRICKS,
+                                                                ModBlocks.BLACK_BRICKS,
+                                                                ModBlocks.RED_BRICKS,
+                                                                ModBlocks.CRACKED_BRICKS,
+                                                                ModBlocks.WHITE_CRACKED_BRICKS,
+                                                                ModBlocks.ORANGE_CRACKED_BRICKS,
+                                                                ModBlocks.PINK_CRACKED_BRICKS,
+                                                                ModBlocks.YELLOW_CRACKED_BRICKS,
+                                                                ModBlocks.LIME_CRACKED_BRICKS,
+                                                                ModBlocks.GREEN_CRACKED_BRICKS,
+                                                                ModBlocks.LIGHT_BLUE_CRACKED_BRICKS,
+                                                                ModBlocks.CYAN_CRACKED_BRICKS,
+                                                                ModBlocks.BLUE_CRACKED_BRICKS,
+                                                                ModBlocks.MAGENTA_CRACKED_BRICKS,
+                                                                ModBlocks.PURPLE_CRACKED_BRICKS,
+                                                                ModBlocks.BROWN_CRACKED_BRICKS,
+                                                                ModBlocks.LIGHT_GRAY_CRACKED_BRICKS,
+                                                                ModBlocks.GRAY_CRACKED_BRICKS,
+                                                                ModBlocks.BLACK_CRACKED_BRICKS,
+                                                                ModBlocks.RED_CRACKED_BRICKS);
+
+    private static Item getSlabVersion(ItemConvertible block) {
+        String block_id = Registries.ITEM.getId(block.asItem()).getPath();
+        String slab_id = block_id.replace("bricks", "brick_slab");
+        return Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, slab_id));
+    }
+
+    private static Item getStairVersion(ItemConvertible block) {
+        String block_id = Registries.ITEM.getId(block.asItem()).getPath();
+        String stair_id = block_id.replace("bricks", "brick_stairs");
+        return Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, stair_id));
+    }
+
+    private static Item getWallVersion(ItemConvertible block) {
+        String block_id = Registries.ITEM.getId(block.asItem()).getPath();
+        String wall_id = block_id.replace("bricks", "brick_wall");
+        return Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, wall_id));
+    }
+
+    private static Item getChiseledVersion(ItemConvertible block) {
+        String block_id = Registries.ITEM.getId(block.asItem()).getPath();
+        String chiseled_id = block_id.replace("bricks", "chiseled_bricks");
+        ColoredBricks.LOGGER.info(Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, chiseled_id)).toString());
+        return Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, chiseled_id));
+    }
 
     private static void offerDyeableRecipes(RecipeExporter exporter, List<ItemConvertible> dyes, List<ItemConvertible> dyeables) {
         for (ItemConvertible dyeable : dyeables) {
@@ -90,8 +149,8 @@ public class ModRecipesProvider extends FabricRecipeProvider {
         String dye_id = Registries.ITEM.getId(dye.asItem()).getPath();
         String dyeable_id = Registries.ITEM.getId(dyeable.asItem()).getPath();
         String dyed_version_id = dye_id.replace("dye", "") + dyeable_id;
-        ColoredBricks.LOGGER.info(Registries.ITEM.get(Identifier.of("coloredbricks", dyed_version_id)).toString());
-        return Registries.ITEM.get(Identifier.of("coloredbricks", dyed_version_id));
+        ColoredBricks.LOGGER.info(Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, dyed_version_id)).toString());
+        return Registries.ITEM.get(Identifier.of(ColoredBricks.MOD_ID, dyed_version_id));
     }
     
     public ModRecipesProvider(FabricDataOutput output, CompletableFuture<WrapperLookup> registriesFuture) {
@@ -104,11 +163,32 @@ public class ModRecipesProvider extends FabricRecipeProvider {
 
         offerDyeableRecipes(exporter, DYES, DYEABLES);
 
+        // Compacting Recipes
         for (Pair<ItemConvertible, ItemConvertible> pair : COMPACT_2BY2_INPUTS_OUTPUTS) {
             ItemConvertible input = pair.getLeft();
             ItemConvertible output = pair.getRight();
 
             offer2x2CompactingRecipe(exporter, RecipeCategory.MISC, output, input);
+        }
+
+        // Slab, Stair, Wall, and Chiseled Block Recipes
+        for (ItemConvertible block : BLOCKS) {
+            ItemConvertible slab = getSlabVersion(block);
+            ItemConvertible stair = getStairVersion(block);
+            ItemConvertible wall = getWallVersion(block);
+
+            offerSlabRecipe(exporter, RecipeCategory.MISC, slab, block);
+            createStairsRecipe(stair, Ingredient.ofItems(block))
+                .criterion(hasItem(block), conditionsFromItem(block))
+                .offerTo(exporter);
+            offerWallRecipe(exporter, RecipeCategory.MISC, wall, block);
+            if (Registries.ITEM.getId(block.asItem()).getPath().contains("cracked")) {
+                continue;
+            } else {
+                ItemConvertible chiseled = getChiseledVersion(block);
+
+                offerChiseledBlockRecipe(exporter, RecipeCategory.MISC, chiseled, slab);
+            }
         }
     }
 
